a8f0ee572cb03b5b12f30e208315e5c9dc7eb876
Dakota Logan <xeliusone@gmail.com>
2019-11-18 10:31:28 -0700

    Final Commit

--- app.js
+++ app.js
@@ -9,14 +9,12 @@ var money = {current:0,total:0},
 			a27:1.1,
 			price:100,
 			perInterval:2,
-			multiplier:1.5,
 			amount:0
 		},
 		upgrade2:{
 			a27:1.1,
 			price:500,
-			perInterval:4,
-			multiplier:1.5,
+			perInterval:10,
 			amount:0
 		}
 	},
@@ -29,24 +27,37 @@ var money = {current:0,total:0},
 		},
 		upgrade2:{
 			a27:1.1,
-			price:500,
+			price:250,
 			multiplier:10,
+			amount:0
+		},
+		upgrade3:{
+			a27:5,
+			price:500,
+			multiplier:2,
 			amount:0
 		}
 	},
 	clickUp1 = clickUp.upgrade1,
 	clickUp2 = clickUp.upgrade2,
+	clickUp3 = clickUp.upgrade3,
 	autoUp1 = autoUp.upgrade1,
 	autoUp2 = autoUp.upgrade2,
-	newUpPrice = function(who){Math.floor(who.price*=(who.a27)); who.a27*=1.05;};
+	newUpPrice = function(who){Math.floor(who.price*=(who.a27)); who.a27*=1.15;};
 	// DOM variables
 var statsMoney = document.getElementById('money'),
 	statsClick = document.getElementById('perClick'),
 	statsAuto = document.getElementById('perSecond'),
 	upgBtnClick1 = document.getElementById('clickUpgrade1'),
+	upgBtnClick1Count = document.getElementById('countC1'),
 	upgBtnClick2 = document.getElementById('clickUpgrade2'),
+	upgBtnClick2Count = document.getElementById('countC2'),
+	upgBtnClick3 = document.getElementById('clickUpgrade3'),
+	upgBtnClick3Count = document.getElementById('countC3'),
 	upgBtnAuto1 = document.getElementById('autoUpgrade1'),
-	upgBtnAuto2 = document.getElementById('autoUpgrade2');
+	upgBtnAuto1Count = document.getElementById('countA1'),
+	upgBtnAuto2 = document.getElementById('autoUpgrade2'),
+	upgBtnAuto2Count = document.getElementById('countA2');
 
 	// Auto upgrade
 	window.setInterval((x=>{money.current+=autoRate;money.total+=autoRate;draw();}), 2500);
@@ -58,12 +69,18 @@ var statsMoney = document.getElementById('money'),
 /* Primary */
 function draw (){
 	priceCheck();
-	autoRate = Math.floor((autoUp1.amount*autoUp1.multiplier*autoUp1.perInterval)+(autoUp2.amount*autoUp2.multiplier*autoUp2.perInterval));
-	clickRate = Math.floor((clickUp1.amount*clickUp1.multiplier)+(clickUp2.amount*clickUp2.multiplier));
+	autoRate = Math.floor((autoUp1.amount*autoUp1.perInterval)+(autoUp2.amount*autoUp2.perInterval));
+	if(clickUp3.amount>0){clickRate = Math.floor(((clickUp1.amount*clickUp1.multiplier)+(clickUp2.amount*clickUp2.multiplier))*(clickUp3.multiplier*clickUp3.amount));}
+		else{clickRate = Math.floor((clickUp1.amount*clickUp1.multiplier)+(clickUp2.amount*clickUp2.multiplier));}
 	// Drawing Stats
 	statsMoney.innerHTML = `${Math.floor(money.current)}`;
 	statsAuto.innerHTML = `${Math.floor(autoRate)}`;
 	statsClick.innerHTML = `${Math.floor(clickRate)}`;
+	upgBtnClick1Count.innerHTML = clickUp1.amount;
+	upgBtnClick2Count.innerHTML = clickUp2.amount;
+	upgBtnClick3Count.innerHTML = clickUp3.amount;
+	upgBtnAuto1Count.innerHTML = autoUp1.amount;
+	upgBtnAuto2Count.innerHTML = autoUp2.amount;
 }
 function priceCheck (){
 	// Click Upgrade 1
@@ -78,6 +95,12 @@ function priceCheck (){
 	}else{
 		upgBtnClick2.disabled = true;
 	}	
+	// Click Upgrade 3
+	if(money.current>=clickUp3.price){
+		upgBtnClick3.disabled = false;
+	}else{
+		upgBtnClick3.disabled = true;
+	}
 	// Auto Upgrade 1
 	if(money.current>=autoUp1.price){
 		upgBtnAuto1.disabled = false;
@@ -104,6 +127,12 @@ function clickAction2(){
 	money.current-=clickUp2.price;
 	clickUp2.amount++;
 	newUpPrice(clickUp2);
+	draw();
+}
+function clickAction3(){
+	money.current-=clickUp3.price;
+	clickUp3.amount++;
+	newUpPrice(clickUp3);
 	draw();
 }
 function autoAction1(){
